037753350841b3c08904d44c1a7d2019064eb596
myersjw1 <myersjw1@appstate.edu>
2023-11-01 16:22:17 -0400

    Start of Lab6

--- inc/DecodeStage.h
+++ inc/DecodeStage.h
@@ -6,6 +6,9 @@
 class DecodeStage: public Stage
 {
    private:
+    void setEInput(PipeReg * ereg, uint64_t stat, uint64_t icode,
+                           uint64_t ifun, uint64_t rA, uint64_t rB,
+                           uint64_t valC, uint64_t valP);
       //TODO: provide declarations for new methods
    public:
       //These are the only methods called outside of the class
--- src/DecodeStage.C
+++ src/DecodeStage.C
@@ -1,5 +1,9 @@
 #include "PipeRegArray.h"
 #include "DecodeStage.h"
+#include "Instruction.h"
+#include "Status.h"
+#include "D.h"
+
 
 /*
  * doClockLow
@@ -12,7 +16,30 @@
  */
 bool DecodeStage::doClockLow(PipeRegArray * pipeRegs)
 {
-   return false;
+    PipeReg * dreg = pipeRegs->getDecodeReg();
+
+    bool mem_error = false;
+    uint64_t icode = Instruction::INOP, ifun = Instruction::FNONE;
+    uint64_t rA = RegisterFile::RNONE, rB = RegisterFile::RNONE;
+    uint64_t valC = 0, valP = 0, stat = Status::SAOK, predPC = 0;
+    bool needvalC = false;
+    bool needregId = false;
+
+    setEInput(dreg, stat, icode, ifun, rA, rB, valC, valP);
+    return false;
+}
+
+void DecodeStage::setEInput(PipeReg * ereg, uint64_t stat, uint64_t icode,
+                           uint64_t ifun, uint64_t rA, uint64_t rB,
+                           uint64_t valC, uint64_t valP)
+{
+   ereg->set(D_STAT, stat);
+   ereg->set(D_ICODE, icode);
+   ereg->set(D_IFUN, ifun);
+   ereg->set(D_RA, rA);
+   ereg->set(D_RB, rB);
+   ereg->set(D_VALC, valC);
+   ereg->set(D_VALP, valP);
 }
 
 /* doClockHigh
@@ -24,7 +51,10 @@ bool DecodeStage::doClockLow(PipeRegArray * pipeRegs)
 */
 void DecodeStage::doClockHigh(PipeRegArray * pipeRegs)
 {
-
+    PipeReg * freg = pipeRegs->getFetchReg();  
+    PipeReg * dreg = pipeRegs->getDecodeReg();
+    freg->normal();
+    dreg->normal();
 }
 
 
