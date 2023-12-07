7534b91c3ea2209b4f6b2a5c894699b747c3b78d
Dylan Shortt <DylanShortt111@icloud.com>
2023-12-07 12:46:35 -0500

    Better HCLS

--- src/ExecuteStage.C
+++ src/ExecuteStage.C
@@ -201,10 +201,11 @@ uint64_t ExecuteStage::aluFunComp(PipeReg * ereg)
 bool ExecuteStage::setCC(PipeReg * ereg, PipeReg * wreg)
 {
 	uint64_t w_stat = wreg->get(W_STAT);
-	if (ereg->get(E_ICODE) == Instruction::IOPQ && 
-		!Stage::m_stat == Status::SADR || !Stage::m_stat == Status::SINS
-		|| !Stage::m_stat == Status::SHLT && !w_stat == Status::SADR
-		|| !w_stat == Status::SINS || !w_stat == Status::SHLT)
+	uint64_t e_icode = ereg->get(E_ICODE);
+	if (e_icode == Instruction::IOPQ && 
+		(!Stage::m_stat == Status::SADR || !Stage::m_stat == Status::SINS
+		|| !Stage::m_stat == Status::SHLT) && (!w_stat == Status::SADR
+		|| !w_stat == Status::SINS || !w_stat == Status::SHLT))
 	{
 		return true;
 	}
@@ -364,9 +365,9 @@ uint64_t ExecuteStage::calculateControlSignals(PipeReg * wreg)
 {
 	uint64_t w_stat = wreg->get(W_STAT);
 
-	if (Stage::m_stat == Status::SADR || Stage::m_stat == Status::SINS 
-		|| Stage::m_stat == Status::SHLT || w_stat == Status::SADR
-		|| w_stat == Status::SINS || w_stat == Status::SHLT)
+	if ((Stage::m_stat == Status::SADR || Stage::m_stat == Status::SINS 
+		|| Stage::m_stat == Status::SHLT) || (w_stat == Status::SADR
+		|| w_stat == Status::SINS || w_stat == Status::SHLT))
 	{
 		return true;
 	}
